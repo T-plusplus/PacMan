389c435c3bb0d8aa97b3a7e9d4d69ff56a067c9f
Tyler <bedert@iastate.edu>
2019-02-27 19:24:20 -0600

    added generation of all pickups in code, based on level

--- PacMan/Assets/Scripts/UIScripts/CoinSpawner.cs
+++ PacMan/Assets/Scripts/UIScripts/CoinSpawner.cs
@@ -5,166 +5,135 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class CoinSpawner : MonoBehaviour {
-
+public class CoinSpawner : MonoBehaviour
+{
     private string LevelName;
-    public Material Material1;
-    //in the editor this is what you would set as the object you wan't to change
-    private GameObject[] Object = new GameObject[62];
-    private int numPowerUpsLeft = 0;
-    private int numCoinsLeft;//TODO change to be determined by whether this is level 2 or another level
-                                   // Use this for initialization
+public Material Material1;
+//in the editor this is what you would set as the object you wan't to change
+private GameObject[] Object = new GameObject[62];
+private int numPowerUpsLeft;
+private int numCoinsLeft;
 
-    void Start() {
-        LevelName = SceneManager.GetActiveScene().name;
-        if (LevelName == "L1")
+private readonly Vector3[] L1pu = { new Vector3(12.5f, .5f, 13), new Vector3(-12.5f, .5f, 13), new Vector3(12.5f, .5f, -7), new Vector3(-12.5f, .5f, -7) };
+private readonly Vector3[] L2pu = { new Vector3(12.5f, .5f, 13), new Vector3(-12.5f, .5f, 13), new Vector3(12.5f, .5f, -13), new Vector3(-12.5f, .5f, -13) };
+private readonly Vector3[] L3pu = { new Vector3(12.5f, .5f, 13), new Vector3(-12.5f, .5f, 13) };
+private readonly string PUpath = "Prefabs/PickUps/";
+
+void Start()
+{
+
+    LevelName = SceneManager.GetActiveScene().name;
+    numCoinsLeft = GameObject.FindGameObjectsWithTag("Coin").Length;
+    numPowerUpsLeft = GameObject.FindGameObjectsWithTag("PowerUp").Length;
+    Object = GameObject.FindGameObjectsWithTag("Wall");
+
+    //Debug.Log(LevelName);
+    if (LevelName == "L1")
+        CreatePrefab(-12.5f, -14, L1pu);
+    else if (LevelName == "L2")
+        CreatePrefab(-12.5f, -14, L2pu);
+    else if (LevelName == "L3")
+        CreatePrefab(-12.5f, -14, L3pu);
+}
+/// <summary>
+/// Takes in any valid point that is part of the hallway, and recursively adds coins that are precisely one space apart. Will occupy all halls.
+/// </summary>
+/// <param name="x">X coordinate of the point to start</param>
+/// <param name="z">Y coordinate of the point to start</param>
+private void CreatePrefab(float x, float z, Vector3[] powerUps)
+{
+    bool isPowerUpLoc = false;
+    if (powerUps != null)
+        for (int i = 0; i < powerUps.Length; i++)
         {
-            numCoinsLeft = GameObject.FindGameObjectsWithTag("Coin").Length;
-            for (int i = 2; i <= 55; i++)
+            if (powerUps[i].x == x && powerUps[i].z == z)
             {
-                Object[i - 1] = GameObject.Find("Cube (" + i + ")");
+                isPowerUpLoc = true;
+                break;
             }
         }
-        if (LevelName == "L2")
-        {
-            numPowerUpsLeft = 4;
-            numCoinsLeft = 282;
-            for (int i = 1; i <= 62; i++)
-            {
-                Object[i - 1] = GameObject.Find("Cube (" + i + ")");
-            }
-        }
-        if (LevelName == "L3")
-        {
-            numCoinsLeft = GameObject.FindGameObjectsWithTag("Coin").Length;
-            for (int i = 1; i <= 61; i++)
-            {
-                Object[i - 1] = GameObject.Find("Cube (" + i + ")");
-            }
-        }
+    GameObject c = (isPowerUpLoc) ? ((GameObject)Instantiate(Resources.Load(PUpath + "PowerUp"))) : ((GameObject)Instantiate(Resources.Load(PUpath + "Coin")));
+    Vector3 pos = new Vector3(x, 0.5f, z);
+    c.transform.position = pos;
+    numCoinsLeft++;
+    //Debug.Log(c);
 
-        //Debug.Log(LevelName);
-        if (LevelName != "L2")
-        {
-            //Debug.Log("Level 3?");
-            CreatePrefab(-12.5f, -14);
+    bool l = Valid(pos, Vector3.left);
+    bool r = Valid(pos, Vector3.right);
+    bool f = Valid(pos, Vector3.forward);
+    bool b = Valid(pos, Vector3.back);
+    //Debug.Log(c.transform.position + " L" + l + " R "+r + " F " + f + " b " + b);
+    if (l)
+        CreatePrefab(x - 1, z, powerUps);
+    if (r)
+        CreatePrefab(x + 1, z, powerUps);
+    if (f)
+        CreatePrefab(x, z + 1, powerUps);
+    if (b)
+        CreatePrefab(x, z - 1, powerUps);
+}
+private void CreatePrefabsFromSave()
+{
 
-        }
-
-    }
-    /// <summary>
-    /// Takes in any valid point that is part of the hallway, and recursively adds coins that are precisely one space apart. Will occupy all halls.
-    /// </summary>
-    /// <param name="x">X coordinate of the point to start</param>
-    /// <param name="z">Y coordinate of the point to start</param>
-    private void CreatePrefab(float x, float z)
+}
+private bool Valid(Vector3 start, Vector3 dir)
+{
+    //Debug.Log("Pos " + pos + "   Dir " + dir);
+    int mask = 1 << LayerMask.NameToLayer("Ignore Raycast");
+    return !Physics.Linecast(start, start + dir, ~mask);
+}
+public void DeductCoin()
+{
+    numCoinsLeft--;
+}
+public void DeductPU()
+{
+    numPowerUpsLeft--;
+}
+public int LeftoverCPU()
+{
+    return numCoinsLeft + numPowerUpsLeft;
+}
+public void OnWin()
+{
+    GameSaveScr gss = GameObject.Find("GameSave").GetComponent<GameSaveScr>();
+    gss.SaveGame();
+    string curLevel = SceneManager.GetActiveScene().name;
+    string nextScene;
+    if (curLevel == "L2")
     {
-        GameObject c = (GameObject)Instantiate(Resources.Load("Coin"));
-        Vector3 pos = new Vector3(x, 0.5f, z);
-        c.transform.position = pos;
-        numCoinsLeft++;
-        //Debug.Log(c);
-
-        bool l = Valid(pos, Vector3.left);
-        bool r = Valid(pos, Vector3.right);
-        bool f = Valid(pos, Vector3.forward);
-        bool b = Valid(pos, Vector3.back);
-        Debug.Log(c.transform.position + " L" + l + " R "+r + " F " + f + " b " + b);
-        if (l)
-            CreatePrefab(x - 1, z);
-        if (r)
-            CreatePrefab(x + 1, z);
-        if (f)
-            CreatePrefab(x, z + 1);
-        if (b)
-            CreatePrefab(x, z - 1);
-    }
-    private void CreatePrefabsFromSave()
-    {
-
-    }
-    private bool Valid(Vector3 start, Vector3 dir)
-    {
-        //Debug.Log("Pos " + pos + "   Dir " + dir);
-        int mask = 1 << LayerMask.NameToLayer("Ignore Raycast");
-        return !Physics.Linecast(start, start + dir, ~mask);//,mask);
-    }
-    public void DeductCoin()
-    {
-        numCoinsLeft--;
-    }
-    public void DeductPU()
-    {
-        numPowerUpsLeft--;
-    }
-    public int LeftoverCPU()
-    {
-        return numCoinsLeft + numPowerUpsLeft;
-    }
-    public void OnWin()
-    {
-        GameSaveScr gss = GameObject.Find("GameSave").GetComponent<GameSaveScr>();
+        nextScene = "L3";
+        gss.CurLevelName = nextScene;
         gss.SaveGame();
-        string curLevel = SceneManager.GetActiveScene().name;
-        string nextScene;
-        if (curLevel == "L2")
-        {
-            nextScene = "L3";
-            gss.CurLevelName = nextScene;
-            gss.SaveGame();
-        }
-        else if (curLevel == "L3")
-        {
-            nextScene = "L1";
-            gss.CurLevelName = nextScene;
-            gss.SaveGame();
-        }
-        else if (curLevel == "L1")
-        {
-            nextScene = "L2";
-            gss.CurLevelName = nextScene;
-            gss.SaveGame();
-        }
-        else
-        {
-            Debug.Log("Error getting next Level, Scene name Input was : " + curLevel);
-            nextScene = "StartMenu_Andr";
-        }
-        
-        StartCoroutine(FlashWhite(nextScene));
-
     }
-
-    IEnumerator FlashWhite(string level)
+    else if (curLevel == "L3")
     {
-        string curLevel = SceneManager.GetActiveScene().name;
-        if (curLevel == "L1")
-        {
-            for (int i = 2; i <= 55; i++)
-            {
-                Object[i - 1].GetComponent<MeshRenderer>().material.color = Color.white;
-            }
-            yield return new WaitForSecondsRealtime(3);
-            SceneManager.LoadScene(level);
-        }
-        if (curLevel == "L2")
-        {
-            for (int i = 1; i <= 62; i++)
-            {
-                Object[i - 1].GetComponent<MeshRenderer>().material.color = Color.white;
-            }
-            yield return new WaitForSecondsRealtime(3);
-            SceneManager.LoadScene(level);
-        }
-        if (curLevel == "L3")
-        {
-            for (int i = 1; i <= 61; i++)
-            {
-                Object[i - 1].GetComponent<MeshRenderer>().material.color = Color.white;
-            }
-            yield return new WaitForSecondsRealtime(3);
-            SceneManager.LoadScene(level);
-        }
-
+        nextScene = "L1";
+        gss.CurLevelName = nextScene;
+        gss.SaveGame();
     }
+    else if (curLevel == "L1")
+    {
+        nextScene = "L2";
+        gss.CurLevelName = nextScene;
+        gss.SaveGame();
+    }
+    else
+    {
+        Debug.Log("Error getting next Level, Scene name Input was : " + curLevel);
+        nextScene = "StartMenu";
+    }
+    StartCoroutine(FlashWhite(nextScene));
+
+}
+
+private IEnumerator FlashWhite(string level)
+{
+    foreach (GameObject o in Object)
+    {
+        o.GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+    yield return new WaitForSecondsRealtime(3);
+    SceneManager.LoadScene(level);
+}
 }
